15a7786976d14282cd0f38f7f2747224452c83fd
Gustavo Pimenta <84296069+GustavoPimenta278@users.noreply.github.com>
2023-05-23 15:35:29 -0300

    atualizando o script de assinaturas
    Atualizando o script para o padrão usado na empresa, porém liberando para o mercado.

--- Corporativo/Generate_Signature_pt2.ps1
+++ Corporativo/Generate_Signature_pt2.ps1
@@ -1,24 +1,20 @@
 <# 
-.SYNOPSIS Configurar Assinatura online do Microsoft Office 365
+.SYNOPSIS Configurar Assinatura do Microsoft Office 365 (local)
 .NOTES	Use por sua conta e risco!
   Version:        1.2
-  Author:         Erich Oliveira https://www.linkedin.com/in/oliveiraerich/
-  Editor: 	  Gustavo Alves Pimenta https://www.linkedin.com/in/gustavo-alves-pimenta-dev/
-.COMPONENT Requires Module MSOnline
+  Author:         	  Erich Oliveira https://www.linkedin.com/in/oliveiraerich/
+  Editor: 		  Gustavo Alves Pimenta https://www.linkedin.com/in/gustavo-alves-pimenta-dev/
+.COMPONENT Requires Module MSOnline and ExchangeOnlineManagement
 #>
 
+##### Conectando ao servidor #####
 
-
-##### FUNCAO CONECTAR #####
-function o365_connect {
-	try {
 		#Armazena na variável $LiveCred os dados de acesso fornecidos por Linha de Comando
 		Write-Host Conectando ao servidor outlook... -ForegroundColor Green
 	     
-    ## Autentica automaticamente  
-	  #$Username = "Seu usuario corporativo"
-		#$Password = ConvertTo-SecureString 'Sua senha corporativa' -AsPlainText -Force
-		#$LiveCred = New-Object System.Management.Automation.PSCredential $Username, $Password
+	    	###$Username = "Usuario com adm"
+		###$Password = ConvertTo-SecureString 'Senha $Username' -AsPlainText -Force
+		###$LiveCred = New-Object System.Management.Automation.PSCredential $Username, $Password
 		
 		#Armazena na variável $cred os dados de acesso fornecidos pelo usuário mediante ao PopUp exibido
 		$LiveCred = get-credential -message "Digite o email com permissoes de administrador do Office 365"
@@ -26,69 +22,58 @@ function o365_connect {
 		#Estabelece conexão com o Office365
 		Connect-MsolService -Credential $LiveCred
 		Connect-ExchangeOnline -Credential $LiveCred
-		
-	}
-	catch {
-	    Write-Host Erro ao conectar, verifique suas credenciais.
-	}
-}
+	
 
-##### CONECTAR #####
-o365_connect
-
-
-
-### test-path ###
+### Verifica a versão do office ###
 $Usuario = [Environment]::UserName
 $Office365Path = 'C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE'
 
 if (!(Test-Path -Path $Office365Path)) {
-	$destinyPath = 'c:\users\' + $usuario + '\appdata\roaming\microsoft\assinaturas' #office 365
+	$destinyPath = 'c:\users\' + $usuario + '\appdata\roaming\microsoft\assinaturas' #office 2013
 
-	} else {
-	    $destinyPath = 'c:\users\' + $usuario + '\appdata\roaming\microsoft\signatures' ##office 2013
-	}
+} else {
+	$destinyPath = 'c:\users\' + $usuario + '\appdata\roaming\microsoft\signatures' ##office 365
+}
 
 if(Test-Path -Path $destinyPath) {
 	Write-Host "Caminho já existe" -ForegroundColor Red
-
-	} Else {
-			Write-Host "Caminho sendo criado" -ForegroundColor Green
-			New-Item -ItemType Directory -Path $destinyPath
+	
+} Else {
+	Write-Host "Caminho sendo criado" -ForegroundColor Green
+	New-Item -ItemType Directory -Path $destinyPath
 			
-			if (Test-Path -Path $destinyPath) {
-				Write-Host "Caminho criado" -ForegroundColor Green
-        
-			} else {
-				Write-Host "Caminho não pode ser criado. Contate o criador do script para analise" -ForegroundColor RED
-			}
+	if (Test-Path -Path $destinyPath) {
+		Write-Host "Caminho criado" -ForegroundColor Green
+
+		} else {
+			Write-Host "Caminho não pode ser criado. Contate o criador do script para analise" -ForegroundColor RED
+	
 		}
-    
+	}
+
 ##### TEMPLATE transfer #####
-$FilesOrigin = "Local de origem dos modelos da assinatura" ##Recomenda-se colocar em um lugar acessivel pelos colaboradores ou oculto mas acessivel.
+$FilesOrigin = "Caminho do Template"
 
 Copy-Item $FilesOrigin -Destination $destinyPath
 
-
 ##### TEMPLATES #####
-$FileHtml = $destinyPath +'\Signature.htm' ##Outlook.exe não aceita muito bem o formato html
+$FileHtml = $destinyPath +'\Signature.htm'
 $FileTxt = $destinyPath + '\Signature.txt'
 
-Write-Host Adicionando assinatura de e-mail -ForegroundColor Green
 
 
 ##### APLICANDO AS ASSINATURAS #####
-$CurrentUserEmail = $Usuario + '@email da empresa'
+Write-Host Adicionando assinatura de e-mail -ForegroundColor Green
+$CurrentUserEmail = $Usuario + '@seu dominio'
 
 $user = Get-MsolUser -UserPrincipalName $CurrentUserEmail
  
 	##### Definindo os campos da assinatura #####
         $UserPrincipalName     = $user.UserPrincipalName       #E-mail
         $DisplayName           = $user.DisplayName             #Full name
-		    $Title				         = $user.Title		 		           #Job Title
+		$Title				   = $user.Title		 		   #Job Title
         $Department            = $user.Department              #Department / Field
-		    $PhoneNumber		       = $user.PhoneNumber			       #Phone Number
-
+		$PhoneNumber		   = $user.PhoneNumber			   #Phone Number
 
 	##### ASSINATURA HTML #####
 	$SignatureHTML = Get-Content -Path $FileHtml -encoding utf8 -ReadCount 0
@@ -100,19 +85,16 @@ $user = Get-MsolUser -UserPrincipalName $CurrentUserEmail
 	$SignatureHTML = $SignatureHTML.Replace("[Department]", $Department)
 	$SignatureHTML = $SignatureHTML.Replace("[UserPrincipalName]", $UserPrincipalName)
 
-	#### Verifica se existe e atribui número de telefone ####
+	#### Verifica e atribui número de telefone ####
 	if ($PhoneNumber.count -gt 0) {
 		$SignatureHTML = $SignatureHTML.Replace("[PhoneNumber]", $PhoneNumber)
 		$SignatureTXT = $SignatureTXT.Replace("[PhoneNumber]", $PhoneNumber)
-
 	}	else {
-		$SignatureHTML = $SignatureHTML.Replace(', Corporativo [PhoneNumber]', "")
-		$SignatureTXT = $SignatureTXT.Replace(', Corporativo [PhoneNumber]', "")
+		$SignatureHTML = $SignatureHTML.Replace(", Corporativo: [PhoneNumber]", "")
+		$SignatureTXT = $SignatureTXT.Replace(", Corporativo: [PhoneNumber]", "")
 
 	}
-
 	##### ASSINATURA TXT #####
-
 	$SignatureTXT = $SignatureTXT.Replace("[DisplayName]", $DisplayName)
 	$SignatureTXT = $SignatureTXT.Replace("[Title]", $Title)
 	$SignatureTXT = $SignatureTXT.Replace("[Department]", $Department)
@@ -122,5 +104,3 @@ $user = Get-MsolUser -UserPrincipalName $CurrentUserEmail
 	$SignatureTXT  | Set-Content -Path $FileTxt
 
 	Write-Host "A assinatura de e-mail do usuario $UserPrincipalName foi atribuida ao Outlook." -ForegroundColor Green
-
-##### DESCONECTAR #####
